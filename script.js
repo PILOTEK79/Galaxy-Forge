ea86000685790818e81dbf31b5ef6567c2f7e35f
Ehsaas <ehsaaskumar92@gmail.com>
2026-06-17 16:08:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,154 @@
 const wallpapers = [
 
-    "uwp4962248.jpeg",
+{
+name:"Astronaut",
+category:"space",
+image:"wp12531994-astronaut-4k-desktop-wallpapers.jpg"
+},
 
-    "uwp5003329.png",
+{
+name:"Galaxy",
+category:"space",
+image:"wp13508917-black-and-white-galaxy-wallpapers.jpg"
+},
 
-    "wp11697464-rgb-room-wallpapers.jpg",
+{
+name:"Landscape",
+category:"aesthetic",
+image:"wp14448617-landscape-4k-aesthetic-wallpapers.jpg"
+},
 
-    "wp12531994-astronaut-4k-desktop-wallpapers.jpg",
+{
+name:"FPS Gaming",
+category:"gaming",
+image:"wp7716162-fps-games-wallpapers.jpg"
+},
 
-    "wp13508917-black-and-white-galaxy-wallpapers.jpg",
+{
+name:"RGB Room",
+category:"gaming",
+image:"wp11697464-rgb-room-wallpapers.jpg"
+},
 
-    "wp14448617-landscape-4k-aesthetic-wallpapers.jpg",
+{
+name:"Lords Of The Fallen",
+category:"gaming",
+image:"wp16052499-lords-of-the-fallen-2-wallpapers.jpg"
+},
 
-    "wp16052499-lords-of-the-fallen-2-wallpapers.jpg",
+{
+name:"Cyber Space",
+category:"space",
+image:"uwp4962248.jpeg"
+},
 
-    "wp7716162-fps-games-wallpapers.jpg"
+{
+name:"Neon Space",
+category:"space",
+image:"uwp5003329.png"
+}
+
 ];
 
-const container =
-document.getElementById("wallpaperContainer");
+const gallery =
+document.getElementById("gallery");
 
-function loadWallpapers() {
+function loadWallpapers(data){
 
-    container.innerHTML = "";
+gallery.innerHTML="";
 
-    wallpapers.forEach(image => {
+data.forEach(item=>{
 
-        container.innerHTML += `
+gallery.innerHTML += `
 
-        <div class="card">
+<div class="card">
 
-            <img
-            src="${image}"
-            alt="Wallpaper">
+<img
+src="${item.image}"
+onclick="openPreview('${item.image}')"
+>
 
-            <a
-            href="${image}"
-            download>
+<h3 style="margin-top:10px">
+${item.name}
+</h3>
 
-            Download
+<a
+href="${item.image}"
+download
+class="download-btn">
 
-            </a>
+Download
 
-        </div>
-        `;
-    });
+</a>
+
+</div>
+`;
+});
 }
 
-function showRandomWallpaper() {
+function filterWallpapers(category){
 
-    const random =
-    wallpapers[
-        Math.floor(
-            Math.random()
-            * wallpapers.length
-        )
-    ];
+if(category==="all"){
 
-    window.open(random);
+loadWallpapers(wallpapers);
+return;
 }
 
-loadWallpapers();
+const filtered =
+wallpapers.filter(
+item=>item.category===category
+);
+
+loadWallpapers(filtered);
+}
+
+function searchWallpaper(){
+
+const search =
+document
+.getElementById("searchInput")
+.value
+.toLowerCase();
+
+const filtered =
+wallpapers.filter(item=>
+item.name
+.toLowerCase()
+.includes(search)
+);
+
+loadWallpapers(filtered);
+}
+
+function openPreview(image){
+
+document.getElementById(
+"previewImg"
+).src=image;
+
+document.getElementById(
+"preview"
+).style.display="flex";
+}
+
+function closePreview(){
+
+document.getElementById(
+"preview"
+).style.display="none";
+}
+
+function showRandomWallpaper(){
+
+const random =
+wallpapers[
+Math.floor(
+Math.random()
+* wallpapers.length
+)
+];
+
+openPreview(random.image);
+}
+
+loadWallpapers(wallpapers);
